96c25bbcb185c933284e9d984fc1f920e4b4612c
cpper <topcpper@gmail.com>
2013-05-12 11:27:45 +0800

    change set<string>* pset to set<string>sset   use const_cast<> to solve the const_iterator

--- test/ppearls/2/1/anagram.cpp
+++ test/ppearls/2/1/anagram.cpp
@@ -10,6 +10,13 @@
  * readfile to set<T>
  * 
  */
+/*error
+ * if the testcode in the main function, or use the heap mem
+ * *** glibc detected *** /home/cpper/workspace/c++/test/ppearls/2/1/anagram:
+ *  double free or corruption (fasttop): 0x091d01a8 ***
+ *  but the sub function does'not return the error
+ */
+
 #include <iostream>
 #include <set>
 #include <algorithm>
@@ -22,27 +29,17 @@ class anagram_node
 {
 public:
     string str ;
-    set<string> * pset;
-    anagram_node():pset(NULL){};
-    ~anagram_node()
-        {
-            cout<<"here is deconstructor"<<endl;
-                                
-            delete pset;
-            
-        }
+    set<string>  sset;
     operator string()
         {
             return str;
         }
-    
     bool operator< (const anagram_node & data) const; // for use of stl sort
-    
 };
     
 set<string> ssec; // the dics
 set<anagram_node> anaset ;
-set<anagram_node> anaset2 ;
+
 // void readdata();
  void dealdata();
 // void output();
@@ -51,68 +48,11 @@ const string get_anagram(const string & data);
 
 int main(int arg ,char *arv[])
 {
-        
-    // cout<<sizeof(anagram_node)<<endl;
-                
-    // anagram_node anadata[2] ;
-    // anadata[0].str="aaa";
-    // anadata[0].pset= new set<string>;
-    // anadata[0].pset->insert("haha");
-    
-    // anadata[1].str="aaa";
-    // anadata[1].pset= new set<string>;
-    // anadata[1].pset->insert("gag");
-
-    // anaset.insert(anadata[0]);
-    // anaset.insert(anadata[1]);
-    // cout<<anaset.size()<<endl;
-    
-        
     // // sort(anadata,anadata+2); 利用set 就不用 sort 了
-    // cout<<(*(anaset.begin())).str<<endl;
-        
-        
-//        cout<<get_anagram(data);
-
     ssec.insert("haha");
     ssec.insert("ahha");
     ssec.insert("ahah");
     dealdata();
-    cout<<anaset.size()<<endl;
-    cout<<(*anaset.begin()).pset->size()<<endl;
-    cout<<*(*anaset.begin()).pset->begin()<<endl;
-    cout<<anaset.begin()->pset<<endl;
-    
-    // anagram_node node1 ;
-    
-    // node1.str="aaga";
-    // node1.pset=new set<string> ;
-    
-    // node1.pset->insert("123");
-    // node1.pset->insert("456");
-    
-    // set<anagram_node> tempset ;
-    // tempset.insert(node1);
-    // cout<<node1.pset<<endl;
-    // cout<<tempset.begin()->pset<<endl;
-
-   cout<<"here is test:"<<endl;
-    
-    anagram_node pnode1 ;
-    
-    (pnode1).str = "haha";
-    (pnode1).pset = new set<string>;
-    (pnode1).pset->insert("haha");
-    anaset2.insert(pnode1);
-    
-    cout<<anaset2.begin()->pset<<endl;
-    
-    cout<<"deleete"<<endl;
-    cout<<anaset2.begin()->pset<<endl;
-    cout<<"test end"<<endl;
-        
-
-    
     
     
         
@@ -127,15 +67,15 @@ void dealdata()
     anagram_node  anatemp ;
     pair< set<anagram_node>::iterator ,bool > pairtemp;
     
+    
     for(set<string>::iterator it = ssec.begin() ; it!=ssec.end() ; it++)
     {
         anatemp.str = get_anagram(*it);
-        anatemp.pset = new set<string>;
-        cout<<anatemp.pset<<endl;
-        
         pairtemp = anaset.insert(anatemp);
-        pairtemp.first->pset->insert(*it);
+        const_cast<anagram_node&>(*(pairtemp.first)).sset.insert(*it);
         
+        
+        //pairtemp.first->sset.insert(*it);
     }
   
 
